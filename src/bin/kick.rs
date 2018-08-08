473d81c2ddf08d0d848f7e4700607c1dfdb13a9a
fredrik.brannbacka <fredrik.brannbacka@gmail.com>
2018-08-08 02:08:46 +0200

    Added missing kick stub

---
+++ src/bin/kick.rs
@@ -0,0 +1,9 @@
+extern crate arnold_rs;
+
+// This is the main function
+fn main() {
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
